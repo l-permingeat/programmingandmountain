5db2a8a51ab8fe48e40c0ed1c1a559401dcc5371
permingeat <laetitia.permingeat@le-campus-numerique.fr>
2022-04-06 16:39:35 +0200

    commit de fin de journée

--- scriptCommun.js
+++ scriptCommun.js
@@ -1,5 +1,10 @@
-$(document).ready(function () {
+let lien = document.getElementById('lien')
+let burger = document.getElementById('burger')
+let ul = document.querySelector('.list')
 
-
-
+/* gestionnaire d'événement sur le a#link pour venir changer l'attribution de la classe .open à la ul et au span#burger */
+lien.addEventListener('click', function(e) {
+  e.preventDefault()
+  burger.classList.toggle('open')
+  ul.classList.toggle('open')
 })
