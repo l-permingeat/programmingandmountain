307ab4689958a66722148055c44a521506e2a133
permingeat <laetitia.permingeat@le-campus-numerique.fr>
2022-04-06 13:29:44 +0200

    Feed = api en javascript

--- scriptIndex.js
+++ scriptIndex.js
@@ -1,15 +1,16 @@
 $(document).ready(function () {
-    $.ajax({
+    function apiJquery() {
+        $.ajax({
 
-        //URL de la requête
-        url: "https://api.spaceflightnewsapi.net/v3/article",
+            //URL de la requête
+            url: "https://api.spaceflightnewsapi.net/v3/articles",
 
-        // méthode d'envoi
-        method: "GET",
+            // méthode d'envoi
+            method: "GET",
 
-        //Format de réponse
-        dataType: "json",
-    })
+            //Format de réponse
+            dataType: "json",
+        })
 
         //En cas de succés 
         .done(function (response) {
@@ -27,10 +28,9 @@ $(document).ready(function () {
             $(".main_marge").prepend('<h1></h1>');
             $(".main_marge h1").text("La requête a échouée");
             //$(".main_marge h1").delay(5000).fadeOut('slow');
-            $(".main_marge h1").delay(5000).fadeOut('slow', function () { 
-                $(".main_marge h1").remove()});
-
-
+            $(".main_marge h1").delay(5000).fadeOut('slow', function () {
+                $(".main_marge h1").remove()
+            });
         })
 
         //code effectué en cas de succés et d'échec
@@ -38,103 +38,39 @@ $(document).ready(function () {
             //alert("requête effectuée");
         })
 
-    /* $.ajax({
-         //URL de la requête
-         url: "https://zenquotes.io/api/quotes/",
- 
-         // méthode d'envoi
-         method: "GET",
- 
-         //Format de réponse
-         dataType: "json",
-     })
-         //En cas de succés 
-         .done(function (response) {
-             //let dataJson = JSON.stringify(response);
-             //$(".article_corps h3").text(response[1].q);
-             //$(".article_corps p").text(response[1].a);
-             //console.log(response);
-             //console.log(response[1]["q"]);
- 
-             function populateArticleTitre(response, i) {
-                 $(".article").append('<div class="article_corps"></div>');
-                 $(".article .article_corps:last").append('<p></p>');
-                 $(".article .article_corps:last p").last().text(response[i].q)
-             }
-             if (response) {
-                 for (var i = 0; i < 7; i++) {
-                     populateArticleTitre(response, i);
- 
-                 }
-             }
-         }) // fin done
- 
-         //en cas d'échec
-         .fail(function (error) {
-             alert("La requête a échouée" + JSON.stringify(error));
-         })
- 
-         //code effectué en cas de succés et d'échec
-         .always(function () {
-             alert("requête effectuée");
-         }) */
+    }//fin function apiJquery
 
-    /* function addApiImg(){
-        $.ajax({
 
-            //URL de la requête
-            url:  "https://api.thecatapi.com/v1/images/search",
-    
-            // méthode d'envoi
-            method: "GET",
-    
-            //Format de réponse
-            dataType: "json",
-        })
-    
-            //En cas de succés 
-            .done(function (response) {
-                
-                function populateArticleImg(response, i) {
-                    $(".article").append('<div class="article_corps"></div>');
-                    $(".article .article_corps:last").append('<img>');
-                    $(".article .article_corps:last img").last().text(response[i].q)
-                }
-    
-                if (response) {
-                    for (var i = 0; i < 7; i++) {
-                        populateArticleImg(response, i);
-                    }
-                }
-    
-            }) // fin done
-    
-            //en cas d'échec
-            .fail(function (error) {
-                alert("La requête a échouée" + JSON.stringify(error));
-            })
-    
-            //code effectué en cas de succés et d'échec
-            .always(function () {
-                alert("requête effectuée");
-            })
-    } */
+    //Bouton actualiser pour remplir le feed
+    $('.refresh').click(function () {
+        apiJquery();
+    });
 
-}); // fin ready function
+    // En attente
+    $(".owl-carousel").owlCarousel({
+        items: 3,
+        margin: 15,
+        autoplay: true,
+        autoWidth: true,
+    })
 
-function populateArticle(response, i) {
-    //créer une classe article_corps
-    $(".article").append('<div class="article_corps"></div>');
-    //image
-    $(".article .article_corps:last").html('<img src="' + response[i].imageUrl + '" alt="">');
-    //  $(".article .article_corps:last img").attr('src',response[i].imageUrl);
-    //titre
-    $(".article .article_corps:last").prepend('<h3></h3>');
-    $(".article .article_corps:last h3").text(response[i].title);
-    //paragraphe
-    $(".article .article_corps:last").append('<p></p>');
-    $(".article .article_corps:last p").text(response[i].summary);
-}
+    function populateArticle(response, i) {
+        //créer une classe article_corps
+        $(".article").append('<div class="article_corps"></div>');
+        //image
+        $(".article .article_corps:last").html('<img src="' + response[i].imageUrl + '" alt="">');
+        //  $(".article .article_corps:last img").attr('src',response[i].imageUrl);
+        //titre
+        $(".article .article_corps:last").prepend('<h3></h3>');
+        $(".article .article_corps:last h3").text(response[i].title);
+        //paragraphe
+        $(".article .article_corps:last").append('<p></p>');
+        $(".article .article_corps:last p").text(response[i].summary);
+    }
+
+}); // fin ready function //
+
+
 
 
 
---
+++ scriptIndexVanilla.js
@@ -0,0 +1,79 @@
+
+let refresh=document.querySelector('.refresh');
+refresh.addEventListener("click",function () {
+    apiVanilla();
+})
+
+function apiVanilla() {
+    fetch("https://api.spaceflightnewsapi.net/v3/articles")
+        .then(response => response.json())
+        .then((json) => { console.log(json), createArticle(json) })
+        .catch((error) => {console.log("erreur : ", error), messageError()})
+
+
+}// fin function apiVanilla
+function messageError(){
+    let main = document.querySelector('.main_marge');
+    let titre = document.createElement("h4");
+    titre.textContent = "La requête a échouée";
+    main.prepend(titre);
+    setTimeout(() => {
+       titre.remove();
+    }, 5000);
+
+}
+//créer les articles
+function createArticle(json) {
+    //selection de mon élément parent
+    if (json) {
+        for (var i = 0; i < 7; i++) {
+            //CREER DIV
+            let article = document.querySelector('.article');
+            //création de la div qui va contenir les articles
+            let article_corps = document.createElement("div");
+            //ajout de la classe à la div
+            article_corps.classList.add("article_corps");
+            //ajout de la div à la div parent
+            article.append(article_corps);
+
+            //CREER TITRE
+            let titre = document.createElement("h3");
+            titre.textContent = json[i].title;
+            article_corps.append(titre);
+
+            //CREER IMAGE
+            let img = document.createElement("img");
+            img.setAttribute('src', json[i].imageUrl)
+            article_corps.append(img);
+
+            //CREER PARAGRAPHE
+            let para = document.createElement("p");
+            para.textContent = json[i].summary;
+            article_corps.append(para);
+
+        }
+    }
+}
+
+async function apiVanillaAttente() {
+    try {
+        //await permet de dire au reste du code d'attendre avant de s'executer 
+        const reponseJS = await fetch("https://api.spaceflightnewsapi.net/v3/articles");
+        // transfrome le json en objet javascript
+        reponseJS = await reponseJS.json();
+        console.log('objet en JS : ', reponseJS);
+    }
+    catch (error) {
+        console.log("erreur : ", error);
+    }
+}
+
+
+
+
+
+
+
+
+
+
