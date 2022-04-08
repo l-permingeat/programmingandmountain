1b084898a8ff2193dc8e5ce550624415e84c0089
permingeat <laetitia.permingeat@le-campus-numerique.fr>
2022-04-08 15:03:16 +0200

    #8 changement couleur des boutons

--- scriptGalerie.js
+++ scriptGalerie.js
@@ -1,29 +1,32 @@
 window.addEventListener("DOMContentLoaded", (event) => {
   let btnColonne = document.querySelector('.btnColonne');
   btnColonne.addEventListener("click", function () {
-    affichageColonneBis()
-    //affichageColonne(btnColonne);
+    affichageColonne("Colonne");
   })
 
   let btnMosaique = document.querySelector('.btnMosaique');
   btnMosaique.addEventListener("click", function () {
-    //affichageColonne(btnMosaique);
-    affichageColonneBisBis();
+    affichageColonne("Mosaique");
   })
 
 });
 
-function affichageColonneBis() {
-  document.getElementById("galerieId").classList.replace("galerieMosaique", "galerieColonne");
-
-}
-function affichageColonneBisBis() {
-  document.getElementById("galerieId").classList.replace("galerieColonne", "galerieMosaique");
-}
 function affichageColonne(typeBouton) {
-  if (typeBouton == "btnColonne") {
+
+  if (typeBouton === "Colonne") {
     document.getElementById("galerieId").classList.replace("galerieMosaique", "galerieColonne");
-  } else  {
+
+    //Changement couleur bouton colonne en gris
+    document.querySelector('.btnColonne').style.backgroundColor = "#6F686D";
+    //Changement couleur bouton mosaïque en bleu
+    document.querySelector('.btnMosaique').style.backgroundColor = "#22577A";
+
+  } else {
     document.getElementById("galerieId").classList.replace("galerieColonne", "galerieMosaique");
+
+    //Changement couleur bouton mosaïque en gris
+    document.querySelector('.btnMosaique').style.backgroundColor = "#6F686D";
+    //Changement couleur bouton colonne en bleu
+    document.querySelector('.btnColonne').style.backgroundColor = "#22577A";
   }
 }
