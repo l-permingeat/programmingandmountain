7494544ab58dfc9f9b5bf1a3dda35011c53613fc
permingeat <laetitia.permingeat@le-campus-numerique.fr>
2022-04-08 10:56:19 +0200

    #5 refacto = retirer la fonction JS dans le HTML pour la mettre dans le fichier JS

--- scriptCommun.js
+++ scriptCommun.js
@@ -1,18 +1,27 @@
-//Ajout de la class show à myDropDown au moment du click pour passer en display block (voir CSS)
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
-}
+window.addEventListener("DOMContentLoaded", (event) => {
+  let dropbtn = document.querySelector('.dropbtn');
+  dropbtn.addEventListener("click", function () {
+    dropDown();
+  })
+});
 
-// event est un objet qui traduit un changement d'état de l'environnement 
-//au click, si le clique n'est pas sur le bouton on enlève la fonction show
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-    let dropdowns = document.getElementsByClassName("dropdown-content");
-    for (let i = 0; i < dropdowns.length; i++) {
-      let openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
+function dropDown() {
+  //Ajout de la class show à myDropDown au moment du click pour passer en display block (voir CSS)
+  document.getElementById("myDropdown").classList.toggle("show");
+
+
+  // event est un objet qui traduit un changement d'état de l'environnement 
+  //au click, si le clique n'est pas sur le bouton on enlève la fonction show
+  window.onclick = function (event) {
+    if (!event.target.matches('.dropbtn')) {
+      let dropdowns = document.getElementsByClassName("dropdown-content");
+      for (let i = 0; i < dropdowns.length; i++) {
+        let openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
       }
     }
   }
-} 
+
+}
